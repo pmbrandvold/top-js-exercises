541406c694bfd8fddc0310b163bdf80be52713e1
Phillip Brandvold <phillip.brandvold@gmail.com>
2018-03-23 14:50:45 -0500

    Passes first test of single letter

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,15 @@
-var caesar = function() {
-
+var caesar = function(string, cypher) {
+  const phrase = Array.from(string);
+  let encrypted = [];
+  phrase.forEach(char => {
+    if (char >= "A" && char <= "z") {
+      char = String.fromCharCode((char.charCodeAt() + cypher));
+      encrypted.push(char);
+    } else {
+      encrypted.push(char);
+    }
+  });
+return encrypted.toString();
 }
 
 module.exports = caesar
