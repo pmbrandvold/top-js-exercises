1c60817879fca788a0a0380a347b347e6587c9a8
Phillip Brandvold <phillip.brandvold@gmail.com>
2018-03-24 21:42:45 -0500

    Breaks down phrases into words, and lists each letter of each word properly.

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,9 +1,23 @@
-function translate() {
-	// body...
+function translate(string) {
+	function translate(string) {
+		var phrase = string.split(' ');
+		phrase.forEach(word => {
+			var letters = Array.from(word);
+			console.log(letters);
+		});
+	}
 }
+	//separate phrase into array split on spaces
+	//separate each index of phrase array by letter
+		//for each
+			//iterate until you find a vowel
+				//split at the vowel
+				//remove everything from before the split and store it in "replace"
+				//add "replace" on to the end of the word
+			//add to newPhrase array
+		//return newPhrase.join(' ');
 
 
 module.exports = {
 	translate
 }
-
