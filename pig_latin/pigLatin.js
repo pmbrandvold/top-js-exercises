066e265695aab923c966a3b0b1b1347924174343
Phillip Brandvold <phillip.brandvold@gmail.com>
2018-03-26 08:30:39 -0500

    Passes four word phrase and single word tests.

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,22 +1,28 @@
 function translate(string) {
 	var vowels = ['a','e','i','o'];
 	var phrase = string.split(' ');
+	var newPhrase = [];
 	phrase.forEach(word => {
 		var letters = Array.from(word);
-		console.log(letters);
+		var beforeVowel = [];
+		var afterVowel = [];
 		letters.forEach((letter, index) => {
 			if (vowels.indexOf(letter) !== -1) {
-				console.log(letter + ' : ' + index);
+				var vowelIndex = index;
+				letters.forEach((letter, index) => {
+					if (index < vowelIndex) {
+						beforeVowel.push(letter);
+					} else {
+						afterVowel.push(letter);
+					}
+				});
 			}
 		});
+		var newWord = afterVowel.join('') + beforeVowel.join('') + 'ay';
+		newPhrase.push(newWord);
 	});
+	return newPhrase.join(' ');
 }
-				//split at the vowel
-				//remove everything from before the split and store it in "replace"
-				//add "replace" on to the end of the word
-			//add to newPhrase array
-		//return newPhrase.join(' ');
-
 
 module.exports = {
 	translate
